296bb5cc221a35d8f28cf556aef6ae894edc5d63
HartChrisT <92996865+HartChrisT@users.noreply.github.com>
2022-04-03 22:37:08 -0400

    finished deliverable 3, onto written analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,18 @@ total_summary = suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),
 lot_summary = suspension_Coil %>% 
   group_by(Manufacturing_Lot) %>%
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Deliverable 3
+# one sample T-test
+suspension_Coil_sample = sample(suspension_Coil$PSI, size = 50)
+t.test(suspension_Coil_sample, mu=mean(suspension_Coil$PSI))
+
+# subset of PSI's for each manufacturing lot
+Manufacturing_lot1 = subset(suspension_Coil,Manufacturing_Lot == "Lot1",select=PSI, drop = T)
+Manufacturing_lot2 = subset(suspension_Coil,Manufacturing_Lot == "Lot2",select=PSI, drop = T)
+Manufacturing_lot3 = subset(suspension_Coil,Manufacturing_Lot == "Lot3",select=PSI, drop = T)
+
+# t-test for each manufacturing lot
+t.test(Manufacturing_lot1, mu=mean(suspension_Coil$PSI))
+t.test(Manufacturing_lot2, mu=mean(suspension_Coil$PSI))
+t.test(Manufacturing_lot3, mu=mean(suspension_Coil$PSI))
